749d31fd2ef923f75c03f7619e4925986654dfe5
Joseph Boyle <boyle.p.joseph@gmail.com>
2019-03-20 17:16:13 -0400

    Update ui.js

--- src/app/constants/ui.js
+++ src/app/constants/ui.js
@@ -1,4 +1,4 @@
-export const PX_BLUE_VERSION = '2.2.1';
+export const PX_BLUE_VERSION = '2.2.2';
 export const UI_UPDATE_TITLE = '@@doc-it/UI_UPDATE_TITLE';
 export const SHOW_MOBILE = '@@doc-it/UI_SHOW_MOBILE';
 export const HIDE_MOBILE = '@@doc-it/UI_HIDE_MOBILE';
