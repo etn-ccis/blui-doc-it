f21b1bfa2c1cf57139e5181fea2253ae66c3941d
joebochill <boyle.p.joseph@gmail.com>
2019-03-26 14:44:03 -0400

    Stackblitz links external

--- src/app/util/showdownExtensions.js
+++ src/app/util/showdownExtensions.js
@@ -33,7 +33,7 @@ export default (config) => {
             regex: /{{\s*link\s*(angular|react)\s*stackblitz=(.+)+\s*}}/g,
             replace: (matchString, framework, url, offset) => {
                 if( (framework === 'react' && (config.react === 'embed' || config.react === 'link')) || (framework === 'angular' && (config.angular === 'embed' || config.angular === 'link')) ){
-                    return  `<div class="stackblitz" data-framework="${framework}"><a href="${url.substring(0, url.indexOf('?'))}"><img src="../images/code.svg" alt="StackBlitz" style="width:24px; display: inline; margin: 0 5px 0 0; transform: translateY(25%);"/>Try the ${framework.substr(0,1).toUpperCase() + framework.substr(1)} StackBlitz example</a></div>`
+                    return  `<div class="stackblitz" data-framework="${framework}"><a href="${url.substring(0, url.indexOf('?'))}" target='_blank' rel='noopener noreferrer'><img src="../images/code.svg" alt="StackBlitz" style="width:24px; display: inline; margin: 0 5px 0 0; transform: translateY(25%);"/>Try the ${framework.substr(0,1).toUpperCase() + framework.substr(1)} StackBlitz example</a></div>`
                 }
                 else if((framework === 'react' && config.react === 'hide') || (framework === 'angular' && config.angular === 'hide')){
                     return ``;
@@ -51,7 +51,7 @@ export default (config) => {
                     </div>`;
                 }
                 else if((framework === 'react' && config.react === 'link') || (framework === 'angular' && config.angular === 'link')){
-                    return `<div class="stackblitz" data-framework="${framework}"><a href="${url.substring(0, url.indexOf('?'))}"><img src="../images/code.svg" alt="StackBlitz" style="width:24px; display: inline; margin: 0 5px 0 0; transform: translateY(25%);"/>Try the ${framework.substr(0,1).toUpperCase() + framework.substr(1)} StackBlitz example</a></div>`;
+                    return `<div class="stackblitz" data-framework="${framework}"><a href="${url.substring(0, url.indexOf('?'))}" target='_blank' rel='noopener noreferrer'><img src="../images/code.svg" alt="StackBlitz" style="width:24px; display: inline; margin: 0 5px 0 0; transform: translateY(25%);"/>Try the ${framework.substr(0,1).toUpperCase() + framework.substr(1)} StackBlitz example</a></div>`;
                 }
                 else if((framework === 'react' && config.react === 'hide') || (framework === 'angular' && config.angular === 'hide')){
                     return ``;
