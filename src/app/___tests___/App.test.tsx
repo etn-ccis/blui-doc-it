6733a2fac43f396fc7c119551e833422aefee378
joebochill <boyle.p.joseph@gmail.com>
2020-02-18 15:40:31 -0500

    Add placeholder test

---
+++ src/app/___tests___/App.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { MainRouter } from '../router';
+import { Reducer } from '../redux/reducers';
+const store = createStore(Reducer());
+
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Provider store={store}><MainRouter /></Provider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
