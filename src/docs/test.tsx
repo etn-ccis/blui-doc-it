7f875569a37503fa3fed87b4cfcc162b72422af5
joebochill <boyle.p.joseph@gmail.com>
2020-02-19 12:28:00 -0500

    Relocate scripts

---
+++ src/docs/test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Test = () => (
+<Typography variant={'h1'} color={'secondary'}>TEST</Typography>
+);
+export default Test
