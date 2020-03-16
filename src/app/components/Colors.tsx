5c430f283780e0df3c3926c7c9972e623968529c
joebochill <boyle.p.joseph@gmail.com>
2020-03-16 14:17:09 -0400

    Adjust style classes

--- src/app/components/Colors.tsx
+++ src/app/components/Colors.tsx
@@ -1,5 +1,5 @@
 import React, { ComponentProps } from 'react';
-import { Typography } from '@material-ui/core';
+import { Typography, makeStyles, createStyles, Theme } from '@material-ui/core';
 import { Bookmark } from '@material-ui/icons';
 import * as Colors from '@pxblue/colors';
 import { AppState } from '../redux/reducers';
@@ -13,7 +13,7 @@ const getColorLabel = (color: string): JSX.Element | null => {
     }
     const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
     return result ? (
-        <div style={{lineHeight: 1}}>
+        <div style={{ lineHeight: 1 }}>
             <div>R: {parseInt(result[1], 16)}</div>
             <div>G: {parseInt(result[2], 16)}</div>
             <div>B: {parseInt(result[3], 16)}</div>
@@ -42,26 +42,67 @@ type SwatchProps = ComponentProps<'div'> & {
     color: string;
     weight: string;
 }
-export const ColorSwatch: React.FC<SwatchProps> = (props): JSX.Element => (
-    <div style={Object.assign({ border: `1px solid ${Colors.black[100]}` }, props.style)}>
-        <div style={{ paddingTop: '100%', color: 'white', background: props.color, position: 'relative' }}>
-            {props.weight === '500' && <Bookmark style={{ top: 8, left: 8, position: 'absolute' }} />}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        swatchWrapper: {
+            border: `1px solid ${Colors.black[100]}`,
+            marginRight: theme.spacing(1), 
+            marginBottom: theme.spacing(1), 
+            flex: '1 1 0px', 
+            maxWidth: 88, 
+            minWidth: 88,
+        },
+        swatch: {
+            paddingTop: '100%',
+            color: Colors.white[50],
+            background: theme.palette.background.paper,
+            position: 'relative'
+        },
+        bookmark: {
+            top: theme.spacing(1),
+            left: theme.spacing(1),
+            position: 'absolute'
+        },
+        label: {
+            background: theme.palette.background.paper,
+            padding: theme.spacing(2),
+        },
+        paletteWrapper: {
+            width: '100%',
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+    })
+);
+
+export const ColorSwatch: React.FC<SwatchProps> = (props): JSX.Element => {
+    const classes = useStyles();
+    const { color, weight, ...otherProps } = props;
+
+    return (
+        <div {...otherProps}>
+            <div className={classes.swatch} style={{ background: color }}>
+                {props.weight === '500' && <Bookmark className={classes.bookmark} />}
+            </div>
+            <div className={classes.label}>
+                <Typography variant={'subtitle2'} style={{ fontWeight: 600 }}>{`${weight}:`}</Typography>
+                <Typography variant={'caption'} style={{ fontFamily: 'Roboto Mono' }}>{getColorLabel(color)}</Typography>
+            </div>
         </div>
-        <div style={{ background: 'white', padding: 16, color: Colors.black[500] }}>
-            <Typography variant={'subtitle2'} style={{ fontWeight: 600 }}>{`${props.weight}:`}</Typography>
-            <Typography variant={'caption'} style={{ fontFamily: 'Roboto Mono' }}>{getColorLabel(props.color)}</Typography>
-        </div>
-    </div>
-)
+    )
+}
 
 type PaletteProps = {
     palette: PXBlueColor;
 }
-export const ColorPalette: React.FC<PaletteProps> = (props): JSX.Element => (
-    <div style={{ width: '100%', display: 'flex', flexWrap: 'wrap' }}>
-        {(Object.keys(props.palette) as Array<keyof PXBlueColor>).filter((key) => parseInt(key as string, 10)).map((key) => (
-            <ColorSwatch key={key} style={{ marginRight: 8, marginBottom: 8, flex: '1 1 0px', maxWidth: 85, minWidth: 85 }} color={props.palette[key] || ''} weight={key as string} />
-        ))}
-    </div>
-);
-
+export const ColorPalette: React.FC<PaletteProps> = (props): JSX.Element => {
+    const classes = useStyles();
+    return (
+        <div className={classes.paletteWrapper}>
+            {(Object.keys(props.palette) as Array<keyof PXBlueColor>).filter((key) => parseInt(key as string, 10)).map((key) => (
+                <ColorSwatch key={key} className={classes.swatchWrapper} color={props.palette[key] || ''} weight={key as string} />
+            ))}
+        </div>
+    );
+}
