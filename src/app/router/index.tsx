cf8a1eece4198d924d31732c5ac29873f7b42439
joebochill <boyle.p.joseph@gmail.com>
2020-03-25 15:38:07 -0400

    Resize fix for carousel

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -68,7 +68,6 @@ export const MainRouter = (): JSX.Element => {
                         <LandingPage />
                     </Route>
                     <Route path="*">
-
                         <>
                             <SharedToolbar title={title} navigationIcon={<Menu />} />
                             <div style={{ minHeight: `calc(50vh - ${toolbarHeight}px)` }}>
