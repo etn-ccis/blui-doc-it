f7f5542f5335c7e19f8ddc8abd304b4d4ee75be0
joebochill <boyle.p.joseph@gmail.com>
2020-03-26 11:28:22 -0400

    Extend base props for page content

--- src/app/components/layout/PageContent.tsx
+++ src/app/components/layout/PageContent.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
-export type PageContentProps = {
+export type PageContentProps = HTMLAttributes<HTMLDivElement> & {
     noPadding?: boolean;
 };
 
-export const PageContent: React.FC<PageContentProps> = (props): JSX.Element => (
-    <div style={{ padding: props.noPadding ? 0 : 20, maxWidth: 1024, margin: '0 auto' }}>{props.children}</div>
-);
+export const PageContent: React.FC<PageContentProps> = (props): JSX.Element => {
+    const { noPadding, children, style, ...other } = props;
+    return (
+        <div style={Object.assign({ padding: noPadding ? 0 : 20, maxWidth: 1024, margin: '0 auto' }, style)} {...other}>{children}</div>
+    );
+}
