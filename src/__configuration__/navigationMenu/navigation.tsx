fff85f5c1abe9ff907761e5f9c42fc095fa4d7f9
joebochill <boyle.p.joseph@gmail.com>
2020-03-27 12:10:40 -0400

    Fix title for iconography page

--- src/__configuration__/navigationMenu/navigation.tsx
+++ src/__configuration__/navigationMenu/navigation.tsx
@@ -213,7 +213,7 @@ export const pageDefinitions: SimpleNavItem[] = [
                 title: 'Iconography',
                 url: '/iconography',
                 component: (
-                    <MarkdownPage title={'Typography'} markdown={Docs.Style.Iconography} background={Colors.gray[50]} />
+                    <MarkdownPage title={'Iconography'} markdown={Docs.Style.Iconography} background={Colors.gray[50]} />
                 ),
             },
             {
