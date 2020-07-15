5788cab35907aac194f692618af5b2c8c4d9a156
huayunh <hhuayun0828@gmail.com>
2020-07-15 15:38:04 -0400

    Add illustration guide; fix keyword count

--- src/docs/style/index.tsx
+++ src/docs/style/index.tsx
@@ -2,5 +2,6 @@ import Themes from './themes.mdx';
 import Typography from './typography.mdx';
 import Color from './color.mdx';
 import Iconography from './iconography.mdx';
+import Illustrations from './illustrations.mdx';
 
-export { Themes, Typography, Color, Iconography };
+export { Themes, Typography, Color, Iconography, Illustrations };
