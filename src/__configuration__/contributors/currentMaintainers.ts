947b813fa36f6da392ecc2660e59c9ff076cbf6c
huayunh <hhuayun0828@gmail.com>
2020-10-06 14:55:19 -0400

    Update my own description

--- src/__configuration__/contributors/currentMaintainers.ts
+++ src/__configuration__/contributors/currentMaintainers.ts
@@ -12,7 +12,7 @@ export const currentMaintainers: Contributor[] = [
         name: 'Huayun Huang',
         description: 'Designer and developer',
         image: HuayunHuang,
-        info: `Huayun is an interesting hybrid between a UX-driven designer and a detailed-oriented developer.`,
+        info: `Huayun is an interesting hybrid between a UX-driven designer and a detailed-oriented developer. She likes spending her spare time on 2D visual arts. Better hurry! She has no mercy in your review requests.`,
         contacts: {
             github: `https://github.com/huayunh`,
             linkedIn: `https://www.linkedin.com/in/huayun-huang/`,
@@ -41,7 +41,8 @@ export const currentMaintainers: Contributor[] = [
         name: 'Jeffery Greiner',
         description: 'Software testing',
         image: JeffeyGreiner,
-        info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.`,
+        info: `Jeff is a perfectionist.  As a kid racing motocross, Jeff learned to make quick decisions on the best line to take in order jump ahead.  As a Software Engineer in Test, Jeff continues to be passionate about quality and the journey that it takes to produce the best results.  Jeff is just as comfortable designing a new application as he is designing a new walk-in closet for his wife, because he loves a challenge.
+        `,
         contacts: {
             github: 'https://github.com/jeffvg',
         },
@@ -59,7 +60,8 @@ export const currentMaintainers: Contributor[] = [
         name: 'Kyle Horchen',
         description: 'Designer',
         image: KyleHorchen,
-        info: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.`,
+        info: `Kyle is a UI/UX designer at EATON. Before EATON, Kyle had the privilege of working with companies such as the NFL, NBA, NHL, PA Lottery, WESCO, COCA Cola, Rite Aid and many more. A die-hard Star Wars fan, Kyle watches The Empire Strikes back at least once a week.
+        `,
         contacts: {
             github: 'https://github.com/Horchen154',
         },
