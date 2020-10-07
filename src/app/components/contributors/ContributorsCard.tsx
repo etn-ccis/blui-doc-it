e4d3f0501353afa7ddbfad359d885a6f39e5b401
huayunh <hhuayun0828@gmail.com>
2020-10-07 15:39:50 -0400

    Modify api; feels not calling out enough, aborting the lovely idea

--- src/app/components/contributors/ContributorsCard.tsx
+++ src/app/components/contributors/ContributorsCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Avatar, Typography, makeStyles, Theme, createStyles, useTheme } from '@material-ui/core';
 import { Contributor } from '../../../__types__';
 import { Person } from '@material-ui/icons';
@@ -25,14 +25,22 @@ const useStyles = makeStyles((theme: Theme) =>
     })
 );
 
-export const ContributorsCard: React.FC<Contributor> = (props) => {
-    const { name, description, image } = props;
+type ContributorsCardProps = Contributor & {
+    /**
+     * Use this icon when no image is supplied
+     * Default to `Person`
+     */
+    icon?: ReactNode;
+};
+
+export const ContributorsCard: React.FC<ContributorsCardProps> = (props) => {
+    const { name, description, image, icon } = props;
     const classes = useStyles(useTheme());
     return (
         <div style={{ display: 'flex', flexDirection: 'row' }}>
             <div>
                 <Avatar src={image} classes={{ colorDefault: classes.colorDefault }}>
-                    <Person />
+                    {icon || <Person />}
                 </Avatar>
             </div>
             <span className={clsx(classes.textContainer, { [classes.noDescription]: !description })}>
