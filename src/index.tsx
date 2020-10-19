e22164272c0c2318478837bac143bf17e43ce42a
huayunh <hhuayun0828@gmail.com>
2020-10-19 18:48:55 -0400

    Move theme to its own folder

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import * as serviceWorker from './serviceWorker';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import * as PXBThemes from '@pxblue/react-themes';
-import * as Colors from '@pxblue/colors';
 import 'typeface-open-sans';
 import 'typeface-roboto-mono';
 import { createStore } from 'redux';
@@ -23,65 +22,12 @@ if (gaID) {
 }
 import { componentsMap } from './__configuration__/markdown/markdownMapping';
 import 'placeholder-loading/src/scss/placeholder-loading.scss';
+import { HalloweenTheme } from './app/themes';
 
 const store = createStore(Reducer());
 
 ReactDOM.render(
-    <MuiThemeProvider
-        theme={createMuiTheme({
-            ...PXBThemes.blueDark,
-            palette: {
-                ...PXBThemes.blueDark.palette,
-                primary: {
-                    light: Colors.orange[300],
-                    main: Colors.orange[500],
-                    dark: Colors.orange[900],
-                },
-                secondary: {
-                    light: Colors.gold[300],
-                    main: Colors.gold[500],
-                    dark: Colors.gold[900],
-                },
-                background: {
-                    default: Colors.darkBlack[100],
-                    paper: Colors.black[900],
-                },
-                text: {
-                    primary: Colors.black[50],
-                    secondary: Colors.black[200],
-                    hint: Colors.black[200],
-                },
-            },
-            overrides: {
-                ...PXBThemes.blueDark.overrides,
-                MuiAppBar: {
-                    colorDefault: {
-                        color: Colors.black[50],
-                        backgroundColor: Colors.black[800],
-                    },
-                    colorPrimary: {
-                        color: Colors.black[50],
-                        backgroundColor: Colors.black[800],
-                    },
-                    colorSecondary: {
-                        color: Colors.white[200],
-                        backgroundColor: Colors.black[900],
-                    },
-                },
-                MuiButton: {
-                    ...PXBThemes.blueDark.overrides?.MuiButton,
-                    outlined: {},
-                    outlinedPrimary: {},
-                },
-                MuiDrawer: {
-                    paper: {
-                        backgroundColor: Colors.darkBlack[300],
-                    },
-                },
-                MuiTabs: {},
-            },
-        })}
-    >
+    <MuiThemeProvider theme={createMuiTheme(HalloweenTheme)}>
         <CssBaseline />
         <Provider store={store}>
             <MDXProvider components={componentsMap}>
