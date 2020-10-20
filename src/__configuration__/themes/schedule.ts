647e640bd85e0fbe08218b6abd071c13a22a20d1
huayunh <hhuayun0828@gmail.com>
2020-10-19 22:00:39 -0400

    Change back to Oct 25

--- src/__configuration__/themes/schedule.ts
+++ src/__configuration__/themes/schedule.ts
@@ -33,7 +33,7 @@ type Schedule = {
 
 export const schedule: Schedule[] = [
     {
-        start: new Date(0, 9, 19), // Oct 19
+        start: new Date(0, 9, 25), // Oct 25
         end: new Date(0, 10, 2), // Nov 02
         config: {
             theme: HalloweenTheme,
