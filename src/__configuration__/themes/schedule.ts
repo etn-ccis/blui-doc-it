3394e6c24e48991277e2f08724d2363e77070d7c
daileytj <daileytj89@gmail.com>
2020-11-17 15:29:14 -0500

    Add thanksgiving theme

--- src/__configuration__/themes/schedule.ts
+++ src/__configuration__/themes/schedule.ts
@@ -4,6 +4,7 @@ import { ChristmasSchedule } from './christmas';
 import { defaultTheme } from './default';
 import { Schedule, SiteConfig } from './types';
 import { DiwaliSchedule } from './diwali';
+import { ThanksgivingSchedule } from './thanksgiving';
 
 /**
  * Add more holiday themes here.
@@ -12,10 +13,16 @@ import { DiwaliSchedule } from './diwali';
  * * May 3 - May 6: May the Forth Day
  * * Oct 14 - Nov 3: Halloween
  * * Nov 12 - Nov 17: Diwali (Changes each year)
- * * Nov 19 - Nov 30: Thanksgiving (changes each year)
+ * * Nov 22 - Nov 29: Thanksgiving (changes each year)
  * * Dec 23 - Dec 26: Christmas
  */
-export const schedule: Schedule[] = [DiwaliSchedule, ChristmasSchedule, HalloweenSchedule, MayTheForthSchedule];
+export const schedule: Schedule[] = [
+    ThanksgivingSchedule,
+    DiwaliSchedule,
+    ChristmasSchedule,
+    HalloweenSchedule,
+    MayTheForthSchedule,
+];
 
 /**
  * @returns a site configuration given the currently applied holiday theme
---
+++ src/__configuration__/themes/thanksgiving.ts
@@ -0,0 +1,68 @@
+import * as PXBThemes from '@pxblue/react-themes';
+import * as Colors from '@pxblue/colors';
+import * as BrandingColors from '@pxblue/colors-branding';
+import Thanksgiving from '../../app/assets/themes/thanksgiving-banner.jpg';
+import turkeyCursor from '../../app/assets/themes/turkey-cursor.png';
+import AppBarTile from '../../app/assets/themes/thanksgiving-appbar-tile.png';
+import { Schedule } from './types';
+
+export const ThanksgivingSchedule: Schedule = {
+    start: new Date(0, 10, 22), // Nov 22
+    end: new Date(0, 10, 29), // Nov 29
+    config: {
+        theme: {
+            ...PXBThemes.blue,
+            palette: {
+                ...PXBThemes.blue.palette,
+                type: 'light',
+                primary: {
+                    light: BrandingColors.rust[100],
+                    main: BrandingColors.rust[500],
+                    dark: BrandingColors.rust[900],
+                },
+                secondary: {
+                    light: BrandingColors.goldenrod[300],
+                    main: BrandingColors.goldenrod[500],
+                    dark: BrandingColors.goldenrod[900],
+                },
+            },
+            overrides: {
+                ...PXBThemes.blue.overrides,
+                MuiAppBar: {
+                    ...PXBThemes.blue.overrides?.MuiAppBar,
+                    colorSecondary: {
+                        color: Colors.white[50],
+                        backgroundColor: BrandingColors.rust[500],
+                        '& .MuiInputBase-root': {
+                            color: Colors.white[50],
+                        },
+                        '& .MuiSelect-icon': {
+                            color: Colors.white[50],
+                        },
+                    },
+                },
+                MuiButton: {
+                    ...PXBThemes.blue.overrides?.MuiButton,
+                    outlined: {},
+                    outlinedPrimary: {},
+                },
+            },
+        },
+        landingPageBanner: {
+            backgroundImage: `url(${Thanksgiving})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center 88%',
+            backgroundColor: BrandingColors.rust[900],
+            cursor: `url("${turkeyCursor}") 0 25, auto`,
+            color: BrandingColors.rust[500],
+        },
+        logoColor: BrandingColors.rust[500],
+        className: 'thanksgiving',
+        appBarBackground: {
+            backgroundImage: `url(${AppBarTile})`,
+            backgroundSize: '300px',
+            backgroundPosition: 'left 75%',
+        },
+    },
+};
