894b55d2b0c2b2e130a50a3220fcc61f635977ca
huayunh <hhuayun0828@gmail.com>
2021-01-05 09:14:54 -0500

    I overcomplicated those; just use cleanup function

--- src/app/hooks/useTOC.tsx
+++ src/app/hooks/useTOC.tsx
@@ -1,28 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useLocation } from 'react-router-dom';
 import { TOGGLE_TOC } from '../redux/actions';
 
 export const useTOC = (toc: boolean): void => {
     const dispatch = useDispatch();
-    const { pathname } = useLocation();
-    const [prevTOCPathname, setPrevTOCPathname] = useState('');
 
     useEffect(() => {
-        dispatch({ type: TOGGLE_TOC, payload: false });
-    }, [pathname]);
-
-    useEffect(() => {
-        if (toc) {
-            dispatch({ type: TOGGLE_TOC, payload: toc });
-            if (pathname !== prevTOCPathname) {
-                setPrevTOCPathname(pathname);
-            }
-        } else {
-            if (pathname !== prevTOCPathname) {
-                dispatch({ type: TOGGLE_TOC, payload: toc });
-            }
-            // do not turn toc off when it is a lifecycle update within the same page
-        }
-    }, [dispatch, toc, pathname]);
+        dispatch({ type: TOGGLE_TOC, payload: toc });
+        return (): void => {
+            dispatch({ type: TOGGLE_TOC, payload: false });
+        };
+    }, [dispatch, toc]);
 };
