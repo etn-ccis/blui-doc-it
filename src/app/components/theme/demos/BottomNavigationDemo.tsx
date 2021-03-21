26d148fdcd8eeeb114abd68709df4f273f77b17e
huayunh <96_mouse@sina.com>
2021-03-21 04:08:24 -0400

    Add bottom navigation

---
+++ src/app/components/theme/demos/BottomNavigationDemo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import { Dashboard } from '@material-ui/icons';
+import { Device, MapMarkerThree } from '@pxblue/icons-mui';
+
+export const BottomNavigationDemo: JSX.Element = (
+    <BottomNavigation value={0} showLabels style={{ maxWidth: 400, width: '100%', margin: 16 }}>
+        <BottomNavigationAction label={'Overview'} icon={<Dashboard />} />
+        <BottomNavigationAction label={'Assets'} icon={<Device />} />
+        <BottomNavigationAction label={'Locations'} icon={<MapMarkerThree />} />
+    </BottomNavigation>
+);
