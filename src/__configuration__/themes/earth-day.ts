7e401868a3ee05e8565522dc1b41c806620bdbd3
huayunh <hhuayun0828@gmail.com>
2021-04-01 22:01:54 -0400

    Make the button pop out more

--- src/__configuration__/themes/earth-day.ts
+++ src/__configuration__/themes/earth-day.ts
@@ -8,7 +8,7 @@ import { Schedule } from './types';
 
 export const EarthDaySchedule: Schedule = {
     start: new Date(0, 3, 20), // Apr 20
-    end: new Date(0, 3, 25), // Apr 24
+    end: new Date(1, 3, 25), // Apr 24
     config: {
         theme: {
             ...PXBThemes.blue,
@@ -19,11 +19,9 @@ export const EarthDaySchedule: Schedule = {
                 },
             },
         },
+        // @ts-ignore
         landingPageBanner: {
-            backgroundImage: `url(${banner})`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center 53%',
+            background: `linear-gradient(transparent 75%, #000d), url(${banner}) center 53% / cover no-repeat`,
             cursor: `url("${cursor}"), auto`,
             color: Colors.white[50],
         },
