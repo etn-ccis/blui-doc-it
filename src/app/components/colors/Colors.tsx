772f05c59a00cf8dad4bfa82935ca20504d412fd
huayunh <hhuayun0828@gmail.com>
2021-04-26 15:59:00 -0400

    Add copy on click functionality

--- src/app/components/colors/Colors.tsx
+++ src/app/components/colors/Colors.tsx
@@ -1,13 +1,14 @@
-import React, { ComponentProps } from 'react';
+import React, { ComponentProps, useCallback, useState } from 'react';
 import { Typography, makeStyles, createStyles, Theme } from '@material-ui/core';
-import { Bookmark } from '@material-ui/icons';
+import { Bookmark, Check } from '@material-ui/icons';
 import * as Colors from '@pxblue/colors';
 import { AppState } from '../../redux/reducers';
+import { copyTextToClipboard } from '../../shared';
 import { useSelector } from 'react-redux';
 import { PXBlueColor } from '@pxblue/types';
+import colorModule from 'color';
 
-const getColorLabel = (color: string): JSX.Element | null => {
-    const format = useSelector((state: AppState) => state.app.colorFormat);
+const getColorLabel = (color: string, format: 'rgb' | 'hex'): JSX.Element | null => {
     if (format === 'hex') {
         return <span>{color}</span>;
     }
@@ -51,6 +52,11 @@ const useStyles = makeStyles((theme: Theme) =>
         label: {
             background: theme.palette.background.paper,
             padding: theme.spacing(1),
+            position: 'relative',
+            '&:hover $copyOnHoverButton': {
+                display: 'flex',
+                cursor: 'pointer',
+            },
         },
         paletteWrapper: {
             width: '100%',
@@ -61,12 +67,37 @@ const useStyles = makeStyles((theme: Theme) =>
                 WebkitJustifyContent: 'space-between',
             },
         },
+        copyOnHoverButton: {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            display: 'none',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: colorModule(theme.palette.background.paper).alpha(0.9).string(),
+        },
     })
 );
 
 export const ColorSwatch: React.FC<SwatchProps> = (props): JSX.Element => {
     const classes = useStyles();
     const { color, weight, ...otherProps } = props;
+    const format = useSelector((state: AppState) => state.app.colorFormat);
+
+    const [copied, setCopied] = useState(false);
+
+    const colorLabel = useCallback(() => getColorLabel(color, format), [color, format]);
+
+    const copyColorToClipboard = useCallback(() => {
+        if (format === 'hex') {
+            copyTextToClipboard(color);
+        } else {
+            copyTextToClipboard(colorModule(color).rgb().string());
+        }
+        setCopied(true);
+    }, [color, format]);
 
     return (
         <div {...otherProps}>
@@ -74,9 +105,31 @@ export const ColorSwatch: React.FC<SwatchProps> = (props): JSX.Element => {
                 {props.weight === '500' && <Bookmark className={classes.bookmark} />}
             </div>
             <div className={classes.label}>
+                <div
+                    className={classes.copyOnHoverButton}
+                    onClick={copyColorToClipboard}
+                    onMouseLeave={(): void => {
+                        setCopied(false);
+                    }}
+                >
+                    {!copied ? (
+                        <Typography variant={'caption'} color={'primary'}>
+                            {format === 'hex' ? 'Copy HEX' : 'Copy RGB'}
+                        </Typography>
+                    ) : (
+                        <Typography
+                            variant={'caption'}
+                            color={'textPrimary'}
+                            style={{ display: 'flex', alignItems: 'center' }}
+                        >
+                            Copied
+                            <Check fontSize={'inherit'} />
+                        </Typography>
+                    )}
+                </div>
                 <Typography variant={'subtitle2'} style={{ fontWeight: 600 }}>{`${weight}:`}</Typography>
                 <Typography variant={'caption'} style={{ fontFamily: 'Roboto Mono' }}>
-                    {getColorLabel(color)}
+                    {colorLabel()}
                 </Typography>
             </div>
         </div>
