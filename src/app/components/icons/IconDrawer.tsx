0ee7344490c95f8202117834371c4d309d574b02
Ekta Ghag <ektasawant99@gmail.com>
2021-06-25 18:52:55 +0530

    add select

--- src/app/components/icons/IconDrawer.tsx
+++ src/app/components/icons/IconDrawer.tsx
@@ -15,9 +15,15 @@ import {
     makeStyles,
     useMediaQuery,
 } from '@material-ui/core';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+
 import { EmptyState, Spacer } from '@pxblue/react-components';
 
 import { GetApp, Close } from '@material-ui/icons';
+// import { Button, Paper, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 import { Pxblue } from '@pxblue/icons-mui';
 
 import { unCamelCase } from '../../shared';
@@ -60,6 +66,12 @@ const useStyles = makeStyles((theme: Theme) => ({
         display: 'flex',
         alignItems: 'center',
     },
+    formControl: {
+        paddingTop: theme.spacing(1),
+        paddingBottom: theme.spacing(2),
+        marginRight: theme.spacing(2),
+        minWidth: 120,
+    },
 }));
 
 export const IconDrawer: React.FC = () => {
@@ -69,12 +81,23 @@ export const IconDrawer: React.FC = () => {
     const history = useHistory();
     const dispatch = useDispatch();
     const classes = useStyles(theme);
+    const [iconSize, setIconSize] = React.useState(24);
+    const [iconColor, setIconColor] = React.useState('black');
     const drawerOpen = useSelector((state: AppState) => state.app.sidebarOpen);
     const sm = useMediaQuery(theme.breakpoints.down('sm'));
 
     const closeDrawer = (): void => {
         history.replace(`${location.pathname}`);
         dispatch({ type: TOGGLE_SIDEBAR, payload: false });
+    };
+
+
+    const handleChange = (event: React.ChangeEvent<{ value: unknown }>): void => {
+        setIconSize(event.target.value as number);
+    };
+
+    const handleColorChange = (event: React.ChangeEvent<{ value: unknown }>): void => {
+        setIconColor(event.target.value as string);
     };
 
     return (
@@ -151,13 +174,41 @@ export const IconDrawer: React.FC = () => {
                                 Download
                             </Typography>
                             <div>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel id="icon-size-select-label">Select a Size:</InputLabel>
+                                    <Select
+                                        labelId="icon-size-select-label"
+                                        id="icon-size-select"
+                                        value={iconSize}
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value={18}>18dp</MenuItem>
+                                        <MenuItem value={24}>24dp</MenuItem>
+                                        <MenuItem value={36}>36dp</MenuItem>
+                                        <MenuItem value={48}>48dp</MenuItem>
+                                    </Select>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel id="icon-color-select-label">Select a Color:</InputLabel>
+                                    <Select
+                                        labelId="icon-color-select-label"
+                                        id="icon-color-select"
+                                        value={iconColor}
+                                        onChange={handleColorChange}
+                                    >
+                                        <MenuItem value={'black'}>Black</MenuItem>
+                                        <MenuItem value={'white'}>White</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+                            <div>
                                 <Button
                                     variant="contained"
                                     color="primary"
                                     style={{ marginRight: theme.spacing(1) }}
                                     startIcon={<GetApp />}
                                     onClick={(): void => {
-                                        void downloadSvg(selectedIcon, 'red', 240);
+                                        void downloadSvg(selectedIcon, iconColor, iconSize);
                                     }}
                                 >
                                     SVG
