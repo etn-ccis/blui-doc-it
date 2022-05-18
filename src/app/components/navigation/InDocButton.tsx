e0f44cf7e2fbe94551989cb6b0e63e93b6fd01c6
huayunh <hhuayun0828@gmail.com>
2022-05-18 07:27:14 -0400

    Add a caption

--- src/app/components/navigation/InDocButton.tsx
+++ src/app/components/navigation/InDocButton.tsx
@@ -1,23 +1,23 @@
-import { Button } from '@material-ui/core';
+import { Button, Typography, useTheme } from '@material-ui/core';
 import React from 'react';
 
 type InDocButtonProps = {
     title: string;
     href: string;
+    caption?: string;
 };
 
 export const InDocButton: React.FC<InDocButtonProps> = (props) => {
-    const { title, href } = props;
+    const { title, href, caption } = props;
+    const theme = useTheme();
     return (
-        <Button
-            href={href}
-            target={'_blank'}
-            variant={'contained'}
-            disableElevation
-            color={'primary'}
-            style={{ marginBottom: 16 }}
-        >
-            {title}
-        </Button>
+        <div style={{ marginBottom: theme.spacing(2) }}>
+            <Button href={href} target={'_blank'} variant={'contained'} disableElevation color={'primary'}>
+                {title}
+            </Button>
+            <Typography variant={'caption'} color={'textSecondary'} paragraph style={{ marginTop: theme.spacing() }}>
+                {caption}
+            </Typography>
+        </div>
     );
 };
