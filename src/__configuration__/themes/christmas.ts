682bce023121f7316b35933ca8c9acbbec62d6e9
Joseph Boyle <boyle.p.joseph@gmail.com>
2022-11-03 13:42:11 -0400

    Update christmas.ts

--- src/__configuration__/themes/christmas.ts
+++ src/__configuration__/themes/christmas.ts
@@ -7,7 +7,7 @@ import { Schedule } from './_types';
 
 export const ChristmasSchedule: Schedule = {
     start: new Date(0, 11, 23), // Dec 23
-    end: new Date(0, 11, 28), // Dec 28
+    end: new Date(0, 11, 26), // Dec 26
     config: {
         theme: {
             ...BLUIThemes.blue,
