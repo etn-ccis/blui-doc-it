83030d852c51b583a2854336f0f1167b0cefcb51
huayunh <hhuayun0828@gmail.com>
2023-04-12 12:47:55 -0400

    Change current release pointer to R28

--- src/__configuration__/roadmap/index.ts
+++ src/__configuration__/roadmap/index.ts
@@ -1,12 +1,17 @@
 import { Quarter, Release } from '../../__types__';
 
-export const CURRENT_RELEASE: Release = 'R27';
+export const CURRENT_RELEASE: Release = 'R28';
 
 export const AVAILABLE_RELEASES: Array<{
     name: Release;
     year: number;
     quarter: Quarter;
 }> = [
+    {
+        name: 'R28',
+        year: 2023,
+        quarter: 'Q2',
+    },
     {
         name: 'R27',
         year: 2023,
