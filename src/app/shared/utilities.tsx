be479f44bb52a6e2789f6df60525bbcaeab803ca
yazadsidhwa-eaton <yazadsidhwa@eaton.com>
2023-05-25 12:46:45 -0400

    Comments fix

--- src/app/shared/utilities.tsx
+++ src/app/shared/utilities.tsx
@@ -43,13 +43,10 @@ function fallbackCopyTextToClipboard(text: string): void {
     textArea.focus();
     textArea.select();
 
-    // document.execCommand('copy');
-
     navigator.clipboard
         .writeText(text)
         .then(() => {
-            //console.log('Text copied to clipboard.');
-            // Perform additional actions here if needed
+            
         })
         .catch((err) => {
             console.error('Failed to copy text: ', err);
