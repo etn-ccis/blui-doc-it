88e6e26512324c3219806a6a51d5992a79769b27
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2026-07-03 12:16:52 +0530

    Prettier fix

--- src/app/components/visualizations/CommunityBanner.tsx
+++ src/app/components/visualizations/CommunityBanner.tsx
@@ -15,8 +15,8 @@ export const CommunityBanner: React.FC = () => (
         icon={<GroupsIcon fontSize="inherit" />}
         sx={{ mt: 1, mb: 3, borderRadius: '4px', alignItems: 'center' }}
     >
-        This content is built and maintained by the Brightlayer UI community and is not part of an official
-        Brightlayer UI package. Explore live examples and source code in the{' '}
+        This content is built and maintained by the Brightlayer UI community and is not part of an official Brightlayer
+        UI package. Explore live examples and source code in the{' '}
         <Link href={COMMUNITY_DOCS_URL} target="_blank" rel="noopener noreferrer" underline="hover">
             Brightlayer UI Community Docs
         </Link>
